93dbfb26ceb7a9317c2d0a1d1757320aa2d4727c
Jacob Pan <zhenjian3g@qq.com>
2018-02-06 19:34:04 +0800

    update: add routine type to post.sh

--- _posts/post.sh
+++ _posts/post.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
-date="date: `date +%Y-%m-%d`"
-
 usage()
 {
     echo "post [-hac]"
 }
 
+#
+# S1: file
+# $2: content
 create()
 {
     echo "---
@@ -15,29 +16,44 @@ title:
 categories: 
 tags: 
 author: Jacob Pan
-$date
+date: $date
 ---
 
 * content
 {:toc}
 
 
+$2
+
 
 > Jacob Pan [( jacobpan3g.github.io/cn )](http://jacobpan3g.github.io/cn)
 " > $1
 }
 
-while getopts "hac:u:" opt
+##### upgrade
+date=`date +%Y-%m-%d`
+
+##### routine
+routine_content="## $date Monday
+## $date Tuesday
+## $date Webnesday
+## $date Thursday
+## $date Friday
+## $date Saturday
+## $date Sunday"
+
+
+while getopts "hac:u:r:" opt
 do
     case $opt in
         h) usage;;
         a) find . -name "*.md";;
         c) create $OPTARG;;
-        u) sed -i "/^date: 20/c $date" $OPTARG;;
+        u) sed -i "/^date: 20/c date: $date" $OPTARG;;
+        r) create $OPTARG "$routine_content";;
         ?) echo "invalid option";;
     esac
     shift
 done
 #shift $(($OPTIND - 1)) 
-
 
